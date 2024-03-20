475dbb056eebbdee9900cf67b203db7bb743a225
Philip Sosnin <fff26@mail.ru>
2024-03-20 13:09:42 +0400

    Add sortHeroes function and tests

---
+++ src/js/__test__/sortHeroes.test.js
@@ -0,0 +1,16 @@
+import { sortHeroes } from '../sortHeroes';
+
+test('should sort heroes by health descending', () => {
+  const input = [
+    { name: 'мечник', health: 10 },
+    { name: 'маг', health: 100 },
+    { name: 'лучник', health: 80 },
+  ];
+  const expected = [
+    { name: 'маг', health: 100 },
+    { name: 'лучник', health: 80 },
+    { name: 'мечник', health: 10 },
+  ];
+
+  expect(sortHeroes(input)).toEqual(expected);
+});
